a699915e88500b85b7e546e156543af0f1992e60
Indranjana <112919974+IndranjanaChatterjee@users.noreply.github.com>
2023-06-04 22:54:15 +0530

    Update script.js

--- country guide/script.js
+++ country guide/script.js
@@ -1,28 +1,31 @@
 
-search.addEventListener("click",()=>
-{
+search.addEventListener("click", () => {
     let country;
-    country=document.getElementById("country").value;
-    let url=`https://restcountries.com/v3.1/name/${country}?fullText=true`;
-    let p=fetch(url);
-    p.then((value)=>
-    {
+    country = document.getElementById("country").value;
+    let url = `https://restcountries.com/v3.1/name/${country}?fullText=true`;
+    let p = fetch(url);
+    p.then((value) => {
         return (value.json());
-    }).then((val)=>
-    {
+    }).then((val) => {
         console.log(val);
-        document.getElementById("text").innerHTML=`<p>Population:${val[0].population}</p>
-        <br>
+        document.getElementById("text").innerHTML = `
+         <p>Name:${val[0].name.common}</p> 
+        <p>Official Name:(${val[0].name.official})</p>
+        <p>Population:${val[0].population}</p>
+        
         <p>Region:${val[0].region}</p>
-        <br>
+        
         <p>Capital:${val[0].capital}</p>
-        <br>
+        
         <p>Continent:${val[0].continents}</p>
-        <br>
+        
         <p>Area covered:${val[0].area}</p>
-        <br>
-        <p>Time Zones:${val[0].timezones}</p>`;
-        document.getElementById("country").value="";
+        
+        <p>Time Zones:${val[0].timezones}</p>
+        
+        `;
+        document.getElementById("f").src=`${val[0].flags.png}`;
+        document.getElementById("country").value = "";
 
     })
 
