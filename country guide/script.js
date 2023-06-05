d804648dc9f737c7795f05124fcf0b72483d8101
Indranjana <112919974+IndranjanaChatterjee@users.noreply.github.com>
2023-06-05 09:02:56 +0530

    Update script.js

--- country guide/script.js
+++ country guide/script.js
@@ -2,13 +2,20 @@
 search.addEventListener("click", () => {
     let country;
     country = document.getElementById("country").value;
-    let url = `https://restcountries.com/v3.1/name/${country}?fullText=true`;
-    let p = fetch(url);
-    p.then((value) => {
-        return (value.json());
-    }).then((val) => {
-        console.log(val);
-        document.getElementById("text").innerHTML = `
+    if (country == '' || country == null) {
+        document.getElementById("text").innerHTML="";
+        document.getElementById("no").style.visibility = "visible";
+        document.getElementById("flag").style.visibility = "hidden";
+    }
+    else {
+        document.getElementById("no").style.visibility="hidden";
+        let url = `https://restcountries.com/v3.1/name/${country}?fullText=true`;
+        let p = fetch(url);
+        p.then((value) => {
+            return (value.json());
+        }).then((val) => {
+            console.log(val);
+            document.getElementById("text").innerHTML = `
          <p>Name:${val[0].name.common}</p> 
         <p>Official Name:(${val[0].name.official})</p>
         <p>Population:${val[0].population}</p>
@@ -24,9 +31,11 @@ search.addEventListener("click", () => {
         <p>Time Zones:${val[0].timezones}</p>
         
         `;
-        document.getElementById("f").src=`${val[0].flags.png}`;
-        document.getElementById("country").value = "";
+            document.getElementById("f").src = `${val[0].flags.png}`;
+            document.getElementById("flag").style.visibility = "visible";
+            document.getElementById("country").value = "";
+        }
 
-    })
+    )}
 
 })
